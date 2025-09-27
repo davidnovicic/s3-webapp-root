244bb1ac9a98f72c6af593c86a7b8e0e4eb50adf
David <165161057+davidnovicic@users.noreply.github.com>
2025-09-27 03:57:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 # module for creating a website using an s3 bucket
 module "website-s3-bucket" {
-  source  = "app.terraform.io/davidnovicic/s3-static-website/aws"
+  source  = "app.terraform.io/davidnovicic/terraform-aws-s3-static-website/aws"
   version = "1.0.0"
   config = {
     bucket_name    = var.aws_bucket_name
@@ -24,6 +24,7 @@ module "website-s3-bucket" {
     }
   }
 }
+
 
 
 
